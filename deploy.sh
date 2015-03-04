c7c868c0e9d1212d9aa16c5dd3db540a1b065d68
forecho <caizhenghai@gmail.com>
2015-03-04 11:30:48 +0800

    添加文章

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 git checkout gh-pages
 git merge master
+git mv README.html index.html
 git add --all
 git commit -am "添加文章"
-git mv README.html index.html
 git push -u origin gh-pages
 git checkout master
